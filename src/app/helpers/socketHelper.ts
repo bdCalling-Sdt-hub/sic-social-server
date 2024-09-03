cd36852a5852ac61a17aa52083a02e24216fb2e2
Nadir Hossain <nadirhossain336@gmail.com>
2024-09-03 12:25:44 +0600

    feat: :sparkles: active friends socket

--- src/app/helpers/socketHelper.ts
+++ src/app/helpers/socketHelper.ts
@@ -1,25 +1,48 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import colors from 'colors';
 import { Server } from 'socket.io';
 import { logger } from '../utils/winstonLogger';
-const activeUsers = new Set(); 
+import { Friend } from '../modules/Friend/friend.model';
+const activeUsers = new Map<string, string>();
 
 const socket = (io: Server) => {
   io.on('connection', socket => {
     logger.info(colors.blue('A user connected'));
 
-    socket.on("active", (userId)=>{
-      activeUsers.add(userId);
+    // Handle user activation (store the userId and socketId)
+    socket.on('active', (userId) => {
+      activeUsers.set(userId, socket.id);
+      logger.info(colors.green(`User ${userId} is now active`));
+    });
+
+    socket.on("activeUsers", async (props)=>{
+
+      const {userId} = JSON.parse(props);
+
+      const friends:any = await Friend.find({ status: 'accepted', recipientId: userId })
+      .populate({
+        path: 'senderId',
+        select: 'fullName avatar',
+      })
+      .select('senderId');
+
+      // Filter friends who are currently active
+      const activeFriends = friends.filter((friend:any) => 
+      activeUsers.has(friend?.senderId?._id.toString()));
+
+      // Send the active friends back to the client
+      socket.emit(`activeFriends`, activeFriends);
     })
-
-    socket.on("activeUsers", (userId)=>{
-      console.log(userId);
-    })
-
-
 
     //disconnect
-    socket.on('disconnect', () => {
+    socket.on('disconnect', async() => {
       logger.info(colors.red('A user disconnect'));
+      const userId = [...activeUsers.entries()].find(([_, id]) => id === socket.id)?.[0];
+      if (userId) {
+        activeUsers.delete(userId);
+        logger.info(colors.yellow(`User ${userId} is now inactive`));
+      }
     });
   });
 };
