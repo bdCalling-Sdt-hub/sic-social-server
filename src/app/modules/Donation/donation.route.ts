dd875943296f6d5f84e5642c07ba9581a79e645d
iibrahiim70 <iibrahiim.dev@gmail.com>
2024-09-04 10:08:15 +0600

    :hammer: update donation route

--- src/app/modules/Donation/donation.route.ts
+++ src/app/modules/Donation/donation.route.ts
@@ -1,4 +1,4 @@
-import { Router } from 'express';
+import { NextFunction, Request, Response, Router } from 'express';
 import { DonationControllers } from './donation.controller';
 import validateAuth from '../../middlewares/validateAuth';
 import { USER_ROLE } from '../User/user.constant';
@@ -16,6 +16,10 @@ router
   .post(
     validateAuth(USER_ROLE.admin, USER_ROLE.superAdmin),
     upload.single('image'),
+    (req: Request, res: Response, next: NextFunction) => {
+      req.body = JSON.parse(req?.body?.data);
+      next();
+    },
     DonationControllers.createDonation,
   );
 
@@ -26,6 +30,10 @@ router
   .patch(
     validateAuth(USER_ROLE.admin, USER_ROLE.superAdmin),
     upload.single('image'),
+    (req: Request, res: Response, next: NextFunction) => {
+      req.body = JSON.parse(req?.body?.data);
+      next();
+    },
     DonationControllers.updateDonationById,
   );
 
